add116445fc2aeaf403c95e8806c7674a27e2c53
Wiola <w.ledwozyw@gmail.com>
2020-12-23 23:02:43 +0100

    create repository

---
+++ src/main/java/com/deviola/tacobench/TacoBenchApplication.java
@@ -0,0 +1,13 @@
+package com.deviola.tacobench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoBenchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoBenchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deviola/tacobench/TacoBenchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deviola.tacobench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoBenchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
