0e8888146b646431f7340db94b2d321455144cec
Wiola <w.ledwozyw@gmail.com>
2020-12-24 01:28:24 +0100

    create homepage

--- src/main/java/com/deviola/tacobench/TacoCloudApplication.java
+++ src/main/java/com/deviola/tacobench/TacoCloudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TacoBenchApplication {
+public class TacoCloudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TacoBenchApplication.class, args);
+		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
 }
